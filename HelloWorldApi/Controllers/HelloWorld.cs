c26724f0ee8e37d4f5b08969d32e813011aed9ae
PrinceMoradiya30 <princemoradia3008@gmail.com>
2025-08-07 19:51:43 +0530

    HelloW

---
+++ HelloWorldApi/Controllers/HelloWorld.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorld : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Hello from .NET web api" });
+        }
+    }
+}
